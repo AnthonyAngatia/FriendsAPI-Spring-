c79fcc5ef6dd09374e34ee4439aa610507d8729d
unknown <anthonyangatia@gmail.com>
2022-05-23 20:45:34 +0300

    Validation of input

--- src/main/java/com/wiredbrain/friends/controller/FriendController.java
+++ src/main/java/com/wiredbrain/friends/controller/FriendController.java
@@ -3,13 +3,19 @@ package com.wiredbrain.friends.controller;
 import com.wiredbrain.friends.model.Friend;
 import com.wiredbrain.friends.service.FriendService;
 import com.wiredbrain.friends.util.ErrorMessages;
+import com.wiredbrain.friends.util.FieldErrorMessageClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.xml.bind.ValidationException;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class FriendController {
@@ -18,18 +24,38 @@ public class FriendController {
     @Autowired
     FriendService friendService;
 
+    //    Without using the validation class
+//    @PostMapping("/friend")
+//    Friend create(@RequestBody Friend friend) throws ValidationException {
+//        if (friend.getId() == 0 && friend.getFirstName() != null && friend.getLastName() != null)
+//            return friendService.save(friend);
+//        else throw new ValidationException("Friends cannot be created");
+////
+//    }
     @PostMapping("/friend")
-    Friend create(@RequestBody Friend friend) throws ValidationException {
-        if (friend.getId() == 0 && friend.getFirstName() != null && friend.getLastName() != null)
-            return friendService.save(friend);
-         else throw new ValidationException("Friends cannot be created");
-//
+    Friend create(@Valid @RequestBody Friend friend) {
+        return friendService.save(friend);
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    List<FieldErrorMessageClass> exceptionHandler(MethodArgumentNotValidException e){
+        List<FieldError> fieldErrorList = e.getBindingResult().getFieldErrors();
+        List<FieldErrorMessageClass> fieldErrorMessages = fieldErrorList.stream().map(
+                fieldError -> new FieldErrorMessageClass(
+                        fieldError.getField(),
+                        fieldError.getDefaultMessage())).collect(Collectors.toList());
+
+        return fieldErrorMessages;
     }
 
 //    @ExceptionHandler(ValidationException.class)
 //    ResponseEntity<String> exceptionHandler(ValidationException e) {
 //        return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
 //    }
+
+    /**
+     * This method is to be shifted to a class(ControllerExceptionHandler) for reusability.
+     **/
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ValidationException.class)
     ErrorMessages exceptionHandler(ValidationException e) {
